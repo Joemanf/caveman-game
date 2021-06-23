e9c4b0ca99c61863d18aa567def24439e26ebc36
Bryan Thomas <joemanf0@gmail.com>
2021-06-23 19:27:43 -0400

    Added basic movement

---
+++ caveman_game/Assets/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Declaring variables
+    // The reason this is a float is for precision
+    [SerializeField] public float moveSpeed;
+    [SerializeField] public Rigidbody2D rb;
+    // WE WILL NEED THIS LATER, DO NOT DELETE IT!
+    // [SerializeField] public Animator animator;
+
+    // The variable that will store the move speed and direction
+    Vector2 moving;
+
+    void Update()
+    {
+        // Gets the direction in positive or negative numbers, depending on key press
+        float mx = Input.GetAxisRaw("Horizontal");
+        float my = Input.GetAxisRaw("Vertical");
+
+        // New Vector2 gets the direction
+        // which is normalized (changes things to 1, -1, or 0)
+        // which is then multiplied by the movespeed (set in the editor)
+        moving = new Vector2(mx, my).normalized * moveSpeed;
+        // the rigidbody's velocity is set to moving (which should return something like [-1, 0])
+        rb.velocity = moving;
+
+        // Animation stuff
+        // WE WILL NEED THIS LATER, DO NOT DELETE IT!
+        // animator.SetFloat("Horizontal", mx);
+        // animator.SetFloat("Vertical", my);
+        // animator.SetFloat("Speed", moving.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
