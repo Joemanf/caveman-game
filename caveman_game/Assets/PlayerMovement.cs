cb237535b50238a083825f603388034cf3f1978c
Bryan Thomas <joemanf0@gmail.com>
2021-06-23 20:19:24 -0400

    Renamed "Movement" to "PlayerMovement"

--- caveman_game/Assets/PlayerMovement.cs
+++ caveman_game/Assets/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     // Declaring variables
     // The reason this is a float is for precision
